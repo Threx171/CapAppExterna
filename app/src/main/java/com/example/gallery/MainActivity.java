25c24c88ff734266171b0619661a36a3c4d97ef0
Threx171 <alejandro_1173@hotmail.com>
2023-12-05 18:26:22 +0100

    thumbnail

--- app/src/main/java/com/example/gallery/MainActivity.java
+++ app/src/main/java/com/example/gallery/MainActivity.java
@@ -8,14 +8,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     ActivityResultLauncher<Intent> someActivityResultLauncher;
+    ActivityResultLauncher<Intent> someActivityResultLauncher2;
 
     public static int RC_PHOTO_PICKER = 0;
     @Override
@@ -37,16 +40,30 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 });
+        someActivityResultLauncher2 = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            // There are no request codes
+                            Intent data = result.getData();
+                            Bundle extras = data.getExtras();
+                            Bitmap imageBitmap = (Bitmap) extras.get("data");
+                            ImageView imageView = findViewById(R.id.imageView);
+                            imageView.setImageBitmap(imageBitmap);
+                        }
+                    }
+                });
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Create Intent
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.setType("image/jpg");
-                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 //Launch activity to get result
-                someActivityResultLauncher.launch(intent);
+                //someActivityResultLauncher.launch(intent);
+                someActivityResultLauncher2.launch(intent);
             }
         });
 
